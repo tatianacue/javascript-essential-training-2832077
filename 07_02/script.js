aa3bffd4cff23d2ea391f1bea5f0d726b372a742
tatianacue <122386464+tatianacue@users.noreply.github.com>
2024-06-14 14:35:34 -0400

    part 7

--- 07_02/script.js
+++ 07_02/script.js
@@ -7,4 +7,8 @@ let item = "flashlight";
 
 const collection = ["Piggy", item, 5, true];
 
+collection[2] = "camera";
+collection[collection.length] = "new item"; //add new item;
+collection[9] = "at the end"; // you can add anywhere in the array
+console.log(collection[8]);
 console.log(collection);
--- 07_03/script.js
+++ 07_03/script.js
@@ -5,16 +5,22 @@
 
 let backpackContents = ["piggy", "headlamp", "pen"];
 
-console.log(backpackContents);
+backpackContents.push("pencil", 5); //adds new items to end of array (pop is opposite)
+backpackContents.unshift("pencil", 5); //add to front of array (shift removes first item)
+console.log(backpackContents.join(", ")); // comma separated list, you can do any parameter
 
-// backpackContents.forEach(function (item) {
-//   item = `<li>${item}</li>`;
-//   console.log(item);
-// });
+backpackContents.forEach(function (item) {
+  // use for each loop to iterate through item
+  item = `<li>${item}</li>`; // manipulate contents in array
+  console.log(item); // prints item in console
+});
 
-// let longItems = backpackContents.find(function (item) {
-//   if (item.length >= 5) {
-//     return item;
-//   }
-// });
-// console.log("longItems:", longItems);
+let longItems = backpackContents.find(function (item) {
+  //first element where condition is met
+  // grabs each item
+  if (item.length >= 5) {
+    // sees if each item is greater than equal to 5
+    return item;
+  }
+});
+console.log("longItems:", longItems);
